f2356738982e390649ee0bdf91f92353976d1050
Travis Bickle <kak.serpom.po.yaitsam@gmail.com>
2010-12-18 04:34:31 +0300

    Signup's makeup.

---
+++ static/js/CmpAccountLogin.js
@@ -0,0 +1,26 @@
+$(function() {
+
+	$('.AccountLoginForm form').ajaxFormController({
+		success: function (result, statusText, xhr, $form) {
+			if (result.success) {
+				var backUrl = $.urlParam('backUrl');
+				if (backUrl != null) {
+				location.href = backUrl;
+			}
+			else {
+				location.reload();
+			}
+		} else {
+			
+			$form.find('.errorMessage').remove();
+			for (var field in result.errors) {
+				
+				$form.find('input[name="'+field+'"]').after('<div class="errorMessage">'+_(result.errors[field])+'</div>');;
+			
+			}
+				
+		}
+	}});
+	$('.AccountLoginForm form').find('button[disabled]').removeAttr('disabled');
+		
+});
