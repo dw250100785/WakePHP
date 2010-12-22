8a08a992337af2a9957ce29a129d6c876c0c1085
kakserpom <kak.serpom.po.yaitsam@gmail.com>
2010-12-22 08:17:56 +0300

    Up.

---
+++ static/js/locale/ui-multiselect-ru.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale ru, ru-RU
+ */
+
+$.extend($.ui.multiselect.locale, {
+	addAll:'Добавить все',
+	removeAll:'Убрать все',
+	itemsCount:'в списке'
+});
