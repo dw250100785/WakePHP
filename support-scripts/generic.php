5729bbb2b63b806027d4147dec77a7e19b8f0ee4
kakserpom <kak.serpom.po.yaitsam@gmail.com>
2010-12-24 20:51:26 +0300

    Up.

--- support-scripts/generic.php
+++ support-scripts/generic.php
@@ -17,7 +17,6 @@ class WakePHP {
 	public function sendPacket($p) {
 		fwrite($this->sock,json_encode($p)."\n");
 	}
-	//public function saveBlock($block) {file_put_contents('./PackagedThemes/simple/blocks/'.$block['name'],json_encode($block));}
 	public function __call($m,$a) {
 		$this->sendPacket(array(
 					'op' => 'singleCall',
