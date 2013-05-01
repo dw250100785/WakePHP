3656aba6ded9339edefffd65f5976ecd10c50e8a
OZ <normandiggs@gmail.com>
2013-05-01 16:51:30 +0400

    twitter button

--- components/CmpAccount.class.php
+++ components/CmpAccount.class.php
@@ -197,7 +197,7 @@ class CmpAccount extends Component {
 					 parse_str($conn->body, $response);
 					 /** @var AuthTokensORM $this->appInstance->authtokens */
 					 if (!isset($response['oauth_token']) || !isset($response['oauth_token_secret'])) {
-						 $this->req->setResult();
+						 $this->req->setResult($conn->body);
 						 return;
 					 }
 					 $this->appInstance->authTokens->addToken($response['oauth_token'], $response['oauth_token_secret'],
