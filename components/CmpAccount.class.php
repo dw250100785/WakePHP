384f38a44030d7ad77d2240fa490355fbd7fcec8
OZ <normandiggs@gmail.com>
2013-05-02 15:34:39 +0400

    twitter button

--- components/CmpAccount.class.php
+++ components/CmpAccount.class.php
@@ -188,6 +188,7 @@ class CmpAccount extends Component {
 		$url          = $this->config->twitter_auth_url->value . 'oauth/request_token';
 		$base_url     = ($_SERVER['HTTPS'] === 'off' ? 'http' : 'https') . '://' . $this->appInstance->config->domain->value;
 		$redirect_url = $base_url . '/component/Account/TwitterAuthRedirect/json';
+		Daemon::log('twitter auth');
 		$this->appInstance->httpclient->post(
 			$url,
 			[],
