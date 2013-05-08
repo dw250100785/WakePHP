9c6797805466c51b5a80a30bcdc885c08b93e9ba
OZ <normandiggs@gmail.com>
2013-05-08 17:38:57 +0400

    namespaces

--- WakePHP/Blocks/BlockAccountProfile.php
+++ WakePHP/Blocks/BlockAccountProfile.php
@@ -1,26 +1,30 @@
-<?php	
+<?php
+namespace WakePHP\Blocks;
+
+use PHPDaemon\Core\ComplexJob;
+
 class BlockAccountProfile extends Block {
 	public function init() {
-		
+
 		$block = $this;
-		$this->req->components->Account->onAuth(function($result) use ($block) {
-			
+		$this->req->components->Account->onAuth(function ($result) use ($block) {
+
 			$req = $block->req;
-			
+
 			if (!$req->account['logged']) {
-				$req->header('Location: /'.$req->locale.'/account/login?backurl='.urlencode($req->attrs->server['REQUEST_URI']));
+				$req->header('Location: /' . $req->locale . '/account/login?backurl=' . urlencode($req->attrs->server['REQUEST_URI']));
 				$req->finish();
 				return;
 			}
-			
-			$job = $req->job = new ComplexJob(function($job) use ($block) {
-					$block->tplvars = $job->results;
-					$job->block->runTemplate();
+
+			$job = $req->job = new ComplexJob(function ($job) use ($block) {
+				$block->tplvars = $job->results;
+				$job->block->runTemplate();
 			});
-			
-			$job->req = $req;
+
+			$job->req   = $req;
 			$job->block = $block;
-      
+
 			/*$job('couponsNumber', function($jobname, $job) {
 				$job->req->appInstance->coupons->countCoupons(function($result) use ($job, $jobname) {
           $job->setResult($jobname, $result['n']);
@@ -29,10 +33,9 @@ class BlockAccountProfile extends Block {
 					'where' => array('account' => $job->req->account['_id'])
 				));
 			});*/
-						
+
 			$job();
-			
+
 		});
 	}
-
 }
