ec36e84a77cacad61e45faa97a7190ee7076c0db
OZ <normandiggs@gmail.com>
2013-05-08 17:48:58 +0400

    namespaces

--- WakePHP/Blocks/BlockACP.php
+++ WakePHP/Blocks/BlockACP.php
@@ -4,7 +4,7 @@ namespace WakePHP\Blocks;
 class BlockACP extends Block {
 
 	public function init() {
-		$this->req->components->Account->onAuth(function () use ($this) {
+		$this->req->components->Account->onAuth(function () {
 			if (!$this->req->account['logged'] || !in_array('Superusers', $this->req->account['aclgroups'])) {
 				$this->req->header('Location: /' . $this->req->locale . '/account/login?backurl=' . urlencode($this->req->attrs->server['REQUEST_URI']));
 				$this->req->finish();
