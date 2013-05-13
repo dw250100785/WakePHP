bb2a0caa2a507fe1202d3d47d5d3c0f809120b25
OZ <normandiggs@gmail.com>
2013-05-13 16:44:57 +0400

    twitter sign in email verification

--- static/js/CmpAccountFinishSignup.js
+++ static/js/CmpAccountFinishSignup.js
@@ -2,34 +2,33 @@ $(function () {
 
 	$('.AccountLoginForm form').each(function (i, form) {
 		var $form = $(form);
-		$form.ajaxFormController({
-			                         success: function (result, statusText, xhr, $form) {
-				                         if (result.success)
-				                         {
-					                         if ((result.needConfirm!=null) && result.needConfirm)
-					                         {
-						                         location.href = '/'+$('html').attr('lang')+'/account/confirm';
-						                         return;
-					                         }
-					                         var backUrl = $.urlParam('backurl');
-					                         if (backUrl!=null)
-					                         {
-						                         location.href = backUrl;
-					                         }
-					                         else
-					                         {
-						                         location.href = '/'+$('html').attr('lang')+'/';
-					                         }
-				                         }
-				                         else
-				                         {
-					                         $('.errorMessage', $form).remove();
-					                         for (var field in result.errors)
-					                         {
-						                         $('input[name="'+field+'"]', $form).after('<div class="errorMessage">'+_(result.errors[field])+'</div>');
-					                         }
-				                         }
-			                         }
+		$form.ajaxFormController({success: function (result, statusText, xhr, $form) {
+			if (result.success)
+			{
+				if ((result.needConfirm!=null) && result.needConfirm)
+				{
+					location.href = '/'+$('html').attr('lang')+'/account/finishSignup';
+					return;
+				}
+				var backUrl = $.urlParam('backurl');
+				if (backUrl!=null)
+				{
+					location.href = backUrl;
+				}
+				else
+				{
+					location.href = '/'+$('html').attr('lang')+'/';
+				}
+			}
+			else
+			{
+				$('.errorMessage', $form).remove();
+				for (var field in result.errors)
+				{
+					$('input[name="'+field+'"]', $form).after('<div class="errorMessage">'+_(result.errors[field])+'</div>');
+				}
+			}
+		}
 		                         });
 		$('button[disabled]', $form).removeAttr('disabled');
 	});
