20a1034cacabf3437e1cfce82b1b27676e59bac7
OZ <normandiggs@gmail.com>
2013-05-15 17:15:17 +0400

    VK

--- WakePHP/ExternalAuthAgents/VK.php
+++ WakePHP/ExternalAuthAgents/VK.php
@@ -4,18 +4,17 @@ namespace WakePHP\ExternalAuthAgents;
 use PHPDaemon\Core\Daemon;
 use WakePHP\Core\Request;
 
-class Вконтактик extends Generic {
+class VK extends Generic {
 	public function auth() {
-		$request_token_url = 'https://www.facebook.com/dialog/oauth/?' .
-				http_build_query(['client_id'     => $this->cmp->config->facebook_app_key->value,
-								  'response_type' => 'code',
-								  'scope'         => 'email',
-								  'redirect_uri'  => $this->req->getBaseUrl() . '/component/Account/ExternalAuthRedirect/json?agent=Facebook']);
 		$this->req->status(302);
 		$this->req->header('Cache-Control: no-cache, no-store, must-revalidate');
 		$this->req->header('Pragma: no-cache');
 		$this->req->header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
-		$this->req->header('Location: ' . $request_token_url);
+		$this->req->header('Location: https://oauth.vk.com/authorize?' .
+						   http_build_query(['client_id'     => $this->cmp->config->vk_app_key->value,
+											 'response_type' => 'code',
+											 'scope'         => 'email',
+											 'redirect_uri'  => $this->req->getBaseUrl() . '/component/Account/ExternalAuthRedirect/json?agent=VK']));
 		$this->req->setResult([]);
 	}
 
@@ -32,10 +31,10 @@ class Вконтактик extends Generic {
 			return;
 		}
 		$this->appInstance->httpclient->get(
-			['https://graph.facebook.com/oauth/access_token',
-				'client_id'     => $this->cmp->config->facebook_app_key->value,
-				'redirect_uri'  => $this->req->getBaseUrl() . '/component/Account/ExternalAuthRedirect/json?agent=Facebook',
-				'client_secret' => $this->cmp->config->facebook_app_secret->value,
+			['https://oauth.vk.com/access_token',
+				'client_id'     => $this->cmp->config->vk_app_key->value,
+				'redirect_uri'  => $this->req->getBaseUrl() . '/component/Account/ExternalAuthRedirect/json?agent=VK',
+				'client_secret' => $this->cmp->config->vk_app_secret->value,
 				'code'          => $code],
 			function ($conn, $success) {
 				if (!$success) {
@@ -44,20 +43,21 @@ class Вконтактик extends Generic {
 					return;
 				}
 				parse_str($conn->body, $response);
-				if (!isset($response['access_token'])) {
+				$user_id      = Request::getString($response['user_id']);
+				$access_token = Request::getString($response['access_token']);
+				if ($user_id === '' || $access_token === '') {
 					$this->req->status(403);
-					$this->req->setResult(['error' => 'no access_token']);
+					$this->req->setResult(['error' => 'no access token or user id']);
 					return;
 				}
 				$this->appInstance->httpclient->get(
-					['https://graph.facebook.com/me',
-						'fields'       => 'id,name,email',
-						'format'       => 'json',
-						'access_token' => $response['access_token']
+					['https://api.vk.com/method/users.get',
+						'uids'         => $user_id,
+						'fields'       => 'screen_name',
+						'access_token' => $access_token
 					],
-					function ($conn, $success) {
+					function ($conn, $success) use ($user_id) {
 						$response = json_decode($conn->body, true);
-						$id       = Request::getString($response['id']);
 						if (!$success || !is_array($response) || empty($id)) {
 							$this->req->status(302);
 							$this->req->header('Location: ' . $this->req->getBaseUrl());
@@ -65,13 +65,10 @@ class Вконтактик extends Generic {
 							return;
 						}
 						$data = [];
-						if (isset($response['name'])) {
-							$data['username'] = Request::getString($response['name']);
+						if (isset($response['screen_name'])) {
+							$data['username'] = Request::getString($response['screen_name']);
 						}
-						if (isset($response['email'])) {
-							$data['email'] = Request::getString($response['email']);
-						}
-						$this->req->components->account->acceptUserAuthentication('facebook', $id, $data,
+						$this->req->components->account->acceptUserAuthentication('VK', $user_id, $data,
 							function () {
 								$this->req->status(302);
 								$this->req->header('Location: ' . $this->req->getBaseUrl());
