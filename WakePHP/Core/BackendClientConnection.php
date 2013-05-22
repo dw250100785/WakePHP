cc99b9d1fc6119faa8dd3cf92e0a0d459230d2ae
OZ <normandiggs@gmail.com>
2013-05-22 14:44:30 +0400

    documentation

--- WakePHP/Core/BackendClientConnection.php
+++ WakePHP/Core/BackendClientConnection.php
@@ -5,37 +5,45 @@ use PHPDaemon\Core\Daemon;
 use PHPDaemon\Network\Connection;
 use WakePHP\Blocks\Block;
 
-class BackendClientConnection extends Connection {
+class BackendClientConnection extends Connection
+{
 
 	public $reqCounter = 0;
+	/** @var Request */
+	protected $requests;
+	protected $queries;
 
-	public function onPacket($p) {
-		if (!is_array($p)) {
+	public function onPacket($p)
+	{
+		if (!is_array($p))
+		{
 			return;
 		}
-		if ($p['type'] === 'propertyUpdated') {
+		if ($p['type']==='propertyUpdated')
+		{
 			$rid = $p['rid'];
-			/** @q where is property `requests` declared?
-			 * in Connection::__get only allowed fields are: 'connected', 'hostReal', 'host', 'port', 'finished', 'alive', 'freed', 'url'
-			 */
-			if (!isset($this->requests[$rid])) {
-				Daemon::log(__METHOD__ . ': Undefined request #' . $rid);
+			if (!isset($this->requests[$rid]))
+			{
+				Daemon::log(__METHOD__.': Undefined request #'.$rid);
 				return;
 			}
 			$req               = $this->requests[$rid];
 			$req->{$p['prop']} = $p['val'];
 		}
-		elseif ($p['type'] === 'readyBlock') {
+		elseif ($p['type']==='readyBlock')
+		{
 			$rid = $p['rid'];
-			if (!isset($this->requests[$rid])) {
-				Daemon::log(__METHOD__ . ': Undefined request #' . $rid);
+			if (!isset($this->requests[$rid]))
+			{
+				Daemon::log(__METHOD__.': Undefined request #'.$rid);
 				return;
 			}
 			/** @var Request $req */
 			$req = $this->requests[$rid];
 			$bid = $p['bid'];
-			if (!isset($req->queries[$bid])) {
-				Daemon::log(__METHOD__ . ': >>>>> Undefined block #' . $rid . '-' . $bid . ': ' . json_encode($p));
+			if (!isset($req->queries[$bid]))
+			{
+				Daemon::log(__METHOD__.': >>>>> Undefined block #'.$rid.'-'.$bid.': '.json_encode($p));
 				return;
 			}
 			$block = $req->queries[$bid];
@@ -45,86 +53,97 @@ class BackendClientConnection extends Connection {
 		}
 	}
 
-	public function onFinish() {
+	public function onFinish()
+	{
 		$this->queries = null;
 	}
 
 	/**
 	 * @param Request $req
 	 */
-	public function beginRequest($req) {
+	public function beginRequest($req)
+	{
 		Daemon::log('beginRequest');
-		if ($this->reqCounter === PHP_INT_MAX) {
+		if ($this->reqCounter===PHP_INT_MAX)
+		{
 			$this->reqCounter = 0;
 		}
 		$id                  = ++$this->reqCounter;
 		$req->rid            = $id;
 		$this->requests[$id] = $req;
 		$this->sendPacket([
-							  'type' => 'startReq',
-							  'rid'  => $id,
-							  'req'  => $req->exportObject(),
-						  ]);
+			'type' => 'startReq',
+			'rid'  => $id,
+			'req'  => $req->exportObject(),
+		]);
 	}
 
-	public function endRequest($req) {
+	public function endRequest($req)
+	{
 		unset($this->requests[$req->rid]);
 		$this->sendPacket([
-							  'type' => 'endRequest',
-							  'rid'  => $req->rid,
-						  ]);
+			'type' => 'endRequest',
+			'rid'  => $req->rid,
+		]);
 	}
 
 	/**
 	 * @param $rid
 	 * @param Block $block
 	 */
-	public function getBlock($rid, $block) {
-		if (!isset($this->requests[$rid])) {
-			Daemon::log(__METHOD__ . ': Unregistered request #' . $rid);
+	public function getBlock($rid, $block)
+	{
+		if (!isset($this->requests[$rid]))
+		{
+			Daemon::log(__METHOD__.': Unregistered request #'.$rid);
 		}
 		$req                = $this->requests[$rid];
 		$bid                = ++$req->queriesCnt;
 		$req->queries[$bid] = $block;
 		//Daemon::log('Creating query '.@json_encode([$rid, $bid, $block['name'], $block['type']]));
 		$this->sendPacket([
-							  'type'  => 'getBlock',
-							  'rid'   => $rid,
-							  'bid'   => $bid,
-							  'block' => $block->exportObject(),
-						  ]);
+			'type'  => 'getBlock',
+			'rid'   => $rid,
+			'bid'   => $bid,
+			'block' => $block->exportObject(),
+		]);
 	}
 
-	public function sendPacket($p) {
+	public function sendPacket($p)
+	{
 		$data = igbinary_serialize($p);
-		$this->write(pack('N', strlen($data)) . $data);
+		$this->write(pack('N', strlen($data)).$data);
 	}
 
 	/**
 	 * Called when new data received.
 	 * @param string $buf New data.
 	 * @return void
+	 * @refactor
 	 */
-	public function stdin($buf) {
+	public function stdin($buf)
+	{
 		/** @q consider declare it as regular field */
 		$this->buf .= $buf;
 
 		start:
 
-		if (strlen($this->buf) < 4) {
+		if (strlen($this->buf) < 4)
+		{
 			return; // not ready yet
 		}
 
 		$u    = unpack('N', $this->buf);
 		$size = $u[1];
 
-		if (strlen($this->buf) < 4 + $size) {
+		if (strlen($this->buf) < 4+$size)
+		{
 			return; // no ready yet;
 		}
 
 		$packet = binarySubstr($this->buf, 4, $size);
 
-		$this->buf = binarySubstr($this->buf, 4 + $size);
+		$this->buf = binarySubstr($this->buf, 4+$size);
 
 		$this->onPacket(igbinary_unserialize($packet));
 
