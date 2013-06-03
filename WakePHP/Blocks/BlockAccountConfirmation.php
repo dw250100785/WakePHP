63886f6e9e82739262b347cc68b6f02622335812
OZ <normandiggs@gmail.com>
2013-06-03 15:14:41 +0400

    use (this) bugfix

--- WakePHP/Blocks/BlockAccountConfirmation.php
+++ WakePHP/Blocks/BlockAccountConfirmation.php
@@ -3,36 +3,47 @@ namespace WakePHP\Blocks;
 
 use PHPDaemon\Request\Generic as Request;
 
-class BlockAccountConfirmation extends Block {
+class BlockAccountConfirmation extends Block
+{
 
-	public function init() {
+	public function init()
+	{
 
-		$this->req->components->Account->onAuth(function ($result) {
-			if (isset($this->req->attrs->request['email'])) {
+		$this->req->components->Account->onAuth(function ($result)
+		{
+			if (isset($this->req->attrs->request['email']))
+			{
 				$email = Request::getString($this->req->attrs->request['email']);
 			}
-			else {
-				if (!$this->req->account['logged']) {
-					$this->req->header('Location: /' . $this->req->locale . '/account/login');
+			else
+			{
+				if (!$this->req->account['logged'])
+				{
+					$this->req->header('Location: /'.$this->req->locale.'/account/login');
 					$this->req->finish();
 					return;
 				}
 				$email = $this->req->account['email'];
 			}
 			$this->assign('status', 'standby');
-			if (!isset($this->req->attrs->request['code'])) {
+			if (!isset($this->req->attrs->request['code']))
+			{
 				$this->runTemplate();
 				return;
 			}
 			$this->req->appInstance->accounts->confirmAccount(array(
-																  'email'            => $email,
-																  'confirmationcode' => trim($this->req->attrs->request['code'])
-															  ), function ($result) use ($this, $email) {
-				if ($result['updatedExisting']) {
+				'email'            => $email,
+				'confirmationcode' => trim($this->req->attrs->request['code'])
+			), function ($result) use ($email)
+			{
+				if ($result['updatedExisting'])
+				{
 					$this->success();
 				}
-				else {
-					$this->req->appInstance->accounts->getAccountByEmail($email, function ($account) {
+				else
+				{
+					$this->req->appInstance->accounts->getAccountByEmail($email, function ($account)
+					{
 
 						$this->assign('status', isset($account['confirmationcode']) ? 'incorrectCode' : ($account ? 'alreadyConfirmed' : 'accountNotFound'));
 						$this->runTemplate();
@@ -42,8 +53,9 @@ class BlockAccountConfirmation extends Block {
 		});
 	}
 
-	public function success() {
-		$this->req->header('Location: /' . $this->req->locale . '/account/profile');
+	public function success()
+	{
+		$this->req->header('Location: /'.$this->req->locale.'/account/profile');
 		$this->req->finish();
 	}
 
--- WakePHP/Blocks/BlockGenericLoggedDep.php
+++ WakePHP/Blocks/BlockGenericLoggedDep.php
@@ -1,15 +1,20 @@
 <?php
 namespace WakePHP\Blocks;
 
-class BlockGenericLoggedDep extends Block {
+class BlockGenericLoggedDep extends Block
+{
 
-	public function init() {
-		$this->req->components->Account->onAuth(function () use ($this) {
-			if (!$this->req->account['logged']) {
-				$this->req->header('Location: /' . $this->req->locale . '/account/login?backurl=' . urlencode($this->req->attrs->server['REQUEST_URI']));
+	public function init()
+	{
+		$this->req->components->Account->onAuth(function ()
+		{
+			if (!$this->req->account['logged'])
+			{
+				$this->req->header('Location: /'.$this->req->locale.'/account/login?backurl='.urlencode($this->req->attrs->server['REQUEST_URI']));
 				$this->req->finish();
 			}
-			else {
+			else
+			{
 				$this->runTemplate();
 			}
 		});
