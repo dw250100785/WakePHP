e5f9b308bb7b59558e4d5358c45371a3fecc133e
OZ <normandiggs@gmail.com>
2013-06-05 16:12:16 +0400

    extAuth JS

--- static/js/angular/controllers.js
+++ static/js/angular/controllers.js
@@ -14,7 +14,19 @@ angular.module('bitfile.controllers', []).
 			$http.put('/route/notSure', {'request_id': request_id});
 		};
 		$scope.init = function (requests) {
-			$scope.requests = angular.fromJson(requests);
+			if (requests) {
+				$scope.requests = angular.fromJson(requests);
+			}
+			else {
+				$scope.requests = [
+					{'id': 1, 'ip': '127.0.0.2', 'time': '11124'},
+					{'id': 2, 'ip': '127.0.0.3', 'time': '11125'},
+					{'id': 3, 'ip': '127.0.0.4', 'time': '11123'},
+					{'id': 4, 'ip': '127.0.0.5', 'time': '11127'},
+					{'id': 5, 'ip': '127.0.0.6', 'time': '11128'},
+					{'id': 6, 'ip': '8.8.8.8', 'time': '11129'}
+				];
+			}
 		};
 	}])
 	.controller('Dashboard', [function () {
--- themes/simple/blocks/accountExtAuth.tpl
+++ themes/simple/blocks/accountExtAuth.tpl
@@ -1,6 +1,6 @@
 {getBlock name="PageStart"}
 <h1 class="i18n" xmlns="http://www.w3.org/1999/html">External authentication</h1>
-<div ng-controller="ExtAuth" ng-init="init({$authRequests|json_encode})">
+<div ng-controller="ExtAuth" ng-init="init()">
 	<div ng-repeat="request in requests | orderBy:'time'">
 		<div>[[request.ip]]</div>
 		<br/>
