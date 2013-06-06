029a33bff3c706607ed46aaaa53ff1237f0b0172
OZ <normandiggs@gmail.com>
2013-06-06 15:14:44 +0400

    backurl

--- static/js/angular/controllers.js
+++ static/js/angular/controllers.js
@@ -23,22 +23,12 @@ angular.module('wakephp.controllers', []).
 //				$http.get('/route/get_requests');
 			$scope.requests = [
 				{'id': 1, 'ip': '127.0.0.2', 'ctime': '11124'},
-				{'id': 2, 'ip': '127.0.0.3', 'ctime': '11125'},
+				{'id': '51b06e3ff77ae87f577761fb', 'ip': '127.0.0.3', 'ctime': '11125'},
 				{'id': 3, 'ip': '127.0.0.4', 'ctime': '11123'},
 				{'id': 4, 'ip': '127.0.0.5', 'ctime': '11127'},
 				{'id': 5, 'ip': '127.0.0.6', 'ctime': '11128'},
 				{'id': 6, 'ip': '8.8.8.8', 'ctime': '11129'}
 			];
-		};
-		$scope.getClass = function(request) {
-			if (request.id==$scope.currentTokenId)
-			{
-				return 'current_request';
-			}
-			else
-			{
-				return 'token_request';
-			}
 		};
 		$scope.delRequestItem = function(request) {
 			$scope.requests.splice($scope.requests.indexOf(request), 1);
